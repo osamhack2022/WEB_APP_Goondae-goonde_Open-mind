d965a51e6da652d1a94f89ed7183f9c02b964ebf
Sinwoo Park <112746031+KimPalZa@users.noreply.github.com>
2022-10-02 13:17:55 +0900

    feature: 클롤링 데이터 로컬 DB저장기능 구현
    #7 클롤링 데이터 로컬 DB에 저장
    1.  sqlite3 모듈 사용 로컬 DB생성
    2. crawler를 통해 얻은 데이터 DB에 저장
    3.  DB에 저장된 총 데이터 수 조회

--- WEB(BE)/crawler/crawling/saveDB.py
+++ WEB(BE)/crawler/crawling/saveDB.py
@@ -1,16 +1,36 @@
 #DB 저장
-'''
+import sqlite3
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', "*.settings")
-import django
 
-django.setup()
-from *.models import *
 
-def add_data(datas):
-    for data in datas:
-        parser_data(name=data['name'],
-                   category=data['category'],
-                   address=data['address'],
-                   number=data['number'],
-                   benefit=data['benefit'],).save()'''
+class saveDB:
+    def __init__(self):
+        self.dbname = 'data.db'
+        self.connect = sqlite3.connect(self.dbname)
+        self.curser = self.connect.cursor()
+        
+    def creat_db(self):
+        self.curser.execute("""CREATE TABLE crawled_data(
+                            name TEXT, 
+                            category TEXT, 
+                            address TEXT, 
+                            number TEXT, 
+                            benefit TEXT)""")
+        
+    def into_db(self, crawled_data):
+        self.curser.executemany("INSERT INTO crawled_data VALUES(:name, :category, :address, :number, :benefit)", crawled_data)
+        self.connect.commit()
+                            
+    def close_db(self):
+        self.connect.close()
+                            
+    def crawled_amount(self):
+        self.curser.execute("SELECT * FROM crawled_data")
+        save_data = self.curser.fetchall()
+        
+        amount = 0
+        for data in save_data:
+        	amount += 1
+        return amount
+
+    
