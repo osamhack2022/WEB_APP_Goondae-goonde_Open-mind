ae4eb24d0b6bb1c55112403a6765384ac0d54e17
Solf <74342496+SolfE@users.noreply.github.com>
2022-10-09 15:33:58 +0000

    feat: Review model make

--- WEB(BE)/locations/models.py
+++ WEB(BE)/locations/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from accounts.models import Profile
+
 
 # Location model, pk = name 
 class Location(models.Model):
@@ -12,3 +16,19 @@ class Location(models.Model):
         # Only read crawled_data table
         managed = False
         db_table = 'crawled_data'
+
+# Review model, pk = id
+class Review(models.Model):
+    Location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='review', verbose_name="업소")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review', verbose_name="게시자")
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, related_name='review', verbose_name="프로필")
+    likes = models.ManyToManyField(User, related_name='like_reviews', blank = True, verbose_name="좋아요")
+    
+    title = models.CharField(max_length=128, verbose_name="제목")
+    content = models.TextField(verbose_name="내용")
+    image = models.ImageField(upload_to='review/', blank = True, verbose_name="이미지")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="등록시간")
+    
+    class Meta:
+        managed = True
+        db_table = 'locations_review'
