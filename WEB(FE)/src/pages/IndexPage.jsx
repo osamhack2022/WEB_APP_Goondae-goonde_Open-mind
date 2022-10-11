264e5c7608fe7de754dc726239aba405ade8a068
woong <inwoong100@gmail.com>
2022-10-11 23:22:48 +0900

    feature: helmet 구현

--- WEB(FE)/src/pages/IndexPage.jsx
+++ WEB(FE)/src/pages/IndexPage.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import FooterContainer from '../containers/common/FooterContainer';
 import HeaderContainer from '../containers/common/HeaderContainer';
 import PlaceListContainer from '../containers/placeList/PlaceListContainer';
@@ -5,6 +6,9 @@ import PlaceListContainer from '../containers/placeList/PlaceListContainer';
 const IndexPage = () => {
   return (
     <>
+      <Helmet>
+        <title> OPEN MIND - 혜택보기 </title>
+      </Helmet>
       <HeaderContainer />
       <PlaceListContainer />
       <FooterContainer />
