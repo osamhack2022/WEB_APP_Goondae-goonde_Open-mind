504722b3a36b31e2ed5664f556eebe275f44077f
woong <inwoong100@gmail.com>
2022-10-11 23:12:44 +0900

    hotfix: 장소 상세 페이지 카테고리 미표시 되는 것 추가

--- WEB(FE)/src/components/place/Place.jsx
+++ WEB(FE)/src/components/place/Place.jsx
@@ -90,7 +90,11 @@ const Place = ({ product, location, reviews }) => {
         <div className='mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24'>
           <div className='lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8'>
             <h1 className='text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl'>
-              {location.name}
+              {location.name}{' '}
+              <span className='text-md sm:text-lg text-gray-400'>
+                {' '}
+                {location.category}
+              </span>
             </h1>
           </div>
 
