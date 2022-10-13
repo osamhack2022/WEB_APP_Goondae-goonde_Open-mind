0ea1b09e92a9844abc9e9facaf738bb19d20c991
Solf <74342496+SolfE@users.noreply.github.com>
2022-10-13 10:20:17 +0000

    feat: Crawler address data change
    1. 카카오 API를 활용하여 부가적인 좌표정보를 함께 제공

--- WEB(BE)/crawler/crawling/data_processing.py
+++ WEB(BE)/crawler/crawling/data_processing.py
@@ -5,21 +5,25 @@ from PyKakao import KakaoLocal
 
 class processing:
     def __init__(self):
+        # kakao rest api key
         self.rest_key = '*'
         self.local = KakaoLocal(self.rest_key)
         
         
     def return_address(self, crawled_data):
         result = []
+        i = 0
         for data in crawled_data:
             raw_address = data['address']
             
             try:    
                 search = self.local.search_address(raw_address)
-                address = search['documents'][0]['road_address']['address_name']
+                address = search['documents'][0]['road_address']
                 data.update({'address': address})
                 result.append(data)
             except:
-                result.append(data)
-                
+                i += 1
+
+                # 에러난 데이터는 제외
+                print('error', i, data['address'])
         return result
