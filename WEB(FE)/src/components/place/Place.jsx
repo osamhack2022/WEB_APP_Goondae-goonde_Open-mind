2364142d7df1b56911dbaaa2ca378ae16bb040dd
woong <inwoong100@gmail.com>
2022-10-17 23:48:29 +0900

    feature: 지도 이동 시 검색 기능
    1. 내 위치에 customOverlay 구현
    2. 지도 이동시 주변 지역 검색

--- WEB(FE)/src/components/place/Place.jsx
+++ WEB(FE)/src/components/place/Place.jsx
@@ -1,45 +1,42 @@
 import { StarIcon } from '@heroicons/react/20/solid';
-import Map from '../map/Map';
-import { RadioGroup } from '@headlessui/react';
 import MapContainer from '../../containers/map/MapContainer';
-import { Link } from 'react-router-dom';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
 const Place = ({ product, location, reviews, setVisible }) => {
+  console.log(location);
   return (
-    <div className='bg-white'>
+    <div className='bg-white mt-[4rem]'>
       <div className='pt-6'>
         <nav aria-label='Breadcrumb'>
           <ol
             role='list'
             className='mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8'
           >
-            {product.breadcrumbs.map((breadcrumb) => (
-              <li key={breadcrumb.id}>
-                <div className='flex items-center'>
-                  <a
-                    href={breadcrumb.href}
-                    className='mr-2 text-sm font-medium text-gray-900'
-                  >
-                    {breadcrumb.name}
-                  </a>
-                  <svg
-                    width={16}
-                    height={20}
-                    viewBox='0 0 16 20'
-                    fill='currentColor'
-                    xmlns='http://www.w3.org/2000/svg'
-                    aria-hidden='true'
-                    className='h-5 w-4 text-gray-300'
-                  >
-                    <path d='M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z' />
-                  </svg>
-                </div>
-              </li>
-            ))}
+            <li>
+              <div className='flex items-center'>
+                <a
+                  href={product.href}
+                  className='mr-2 text-sm font-medium text-gray-900'
+                >
+                  {location.category}
+                </a>
+                <svg
+                  width={16}
+                  height={20}
+                  viewBox='0 0 16 20'
+                  fill='currentColor'
+                  xmlns='http://www.w3.org/2000/svg'
+                  aria-hidden='true'
+                  className='h-5 w-4 text-gray-300'
+                >
+                  <path d='M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z' />
+                </svg>
+              </div>
+            </li>
+
             <li className='text-sm'>
               <a
                 href={product.href}
