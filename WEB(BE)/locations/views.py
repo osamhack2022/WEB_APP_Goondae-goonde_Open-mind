c5d3cb966568ae596b213978f181ca6cdc5e475c
Solf <74342496+SolfE@users.noreply.github.com>
2022-10-19 05:01:08 +0000

    feat: location list, detail review list,detali 기능수정
    1. location db table명 변경
    2. review url구조 변경
    3. review location_id명과과 작성자를 명시해야했던 버그 수정

--- WEB(BE)/locations/views.py
+++ WEB(BE)/locations/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth import models
 
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, permission_classes
@@ -10,38 +11,52 @@ from accounts.models import Profile
 
 from locations.models import Location, Review
 from locations.serializers import LocationSerializer, ReviewSerializer, ReviewCreateSerializer
-from locations.permissions import ReviewPermission
 
 
 class LocationViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = []
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['category', 'region1', 'region2', 'region3']
-    
     serializer_class = LocationSerializer
     queryset = Location.objects.all()
 
+#class LocationUserStar(APIView)
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all()
     permission_classes = [ReviewPermission]
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['location']
     
+    def get_queryset(self):
+        return Review.objects.filter(location=self.kwargs['location_id'])
+
     def get_serializer_class(self):
-        if self.action == 'list' or 'retrieve':
+        if self.action == ('list' or 'retrieve'):
             return ReviewSerializer
         return ReviewCreateSerializer
     
     def perform_create(self, serializer):
-        location = Location.objects.get(id=self.request.data['location_id'])
+        self.request.user = models.User.objects.get(id=1)
+        location = Location.objects.get(id=self.kwargs['location_id'])
         profile = Profile.objects.get(user=self.request.user)
-        serializer.save(location=location, author=self.request.user, profile=profile)
+        serializer.save(location= location, author=self.request.user, profile=profile)
+    
+    # def create(self, request, **kwargs):
+    #     serializer = ReviewCreateSerializer(data=request.data)
+    #     location = Location.objects.get(id=self.kwargs['location_id'])
+    #     self.request.user = models.User.objects.get(id=1)
+    #     profile = Profile.objects.get(user=self.request.user)
+    #     if serializer.is_valid():
+    #         serializer.save(author=self.request.user, profile=profile, location=location)
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     else:
+    #         return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+
         
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def like_review(request, pk):
-    review = get_object_or_404(Review, pk=pk)
+def like_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
     if request.user in review.likes.all():
         review.likes.remove(request.user)
     else:
