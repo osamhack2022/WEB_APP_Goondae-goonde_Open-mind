c5dd5cf7f660e4760d9382ae6b287419516da79a
Solf <74342496+SolfE@users.noreply.github.com>
2022-10-19 16:19:52 +0000

    feat: location review 좋아요기능 구현

--- WEB(BE)/locations/serializers.py
+++ WEB(BE)/locations/serializers.py
@@ -4,7 +4,7 @@ from accounts.serializers import ProfileSerializer
 
 from locations.models import Location, Review, LocationUserStar
 
-
+# location
 class LocationListSerializer(serializers.ModelSerializer):
     total_reviews = serializers.IntegerField(source='review.count', read_only=True)
     total_likes = serializers.IntegerField(source='likes.count', read_only=True)
@@ -38,19 +38,46 @@ class LocationDetailSerializer(serializers.ModelSerializer):
             if request.user in obj.likes.all():
                 return True
         return False
-        
+
+# location_star
 class LocationUserStarSerializer(serializers.ModelSerializer):
     class Meta:
         model = LocationUserStar
-        fields = '__all__'
+        fields = ['rate']
 
 
-class ReviewSerializer(serializers.ModelSerializer):
+# location_review
+class ReviewListSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer(read_only=True)
+    total_likes = serializers.IntegerField(source='likes.count', read_only=True)
+    user_liked = serializers.SerializerMethodField('get_user_liked_toggle')
+
+    class Meta:
+        model = Review
+        fields = ["id", "location", "author", "profile", "content", "image", "created_at", "user_liked", "total_likes"]
+    
+    def get_user_liked_toggle(self, obj):
+        request =  self.context.get('request', None)
+        if request:
+            if request.user in obj.likes.all():
+                return True
+        return False
+
+class ReviewDetailSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    total_likes = serializers.IntegerField(source='likes.count', read_only=True)
+    user_liked = serializers.SerializerMethodField('get_user_liked_toggle')
 
     class Meta:
         model = Review
         fields = '__all__'
+    
+    def get_user_liked_toggle(self, obj):
+        request =  self.context.get('request', None)
+        if request:
+            if request.user in obj.likes.all():
+                return True
+        return False
 
 class ReviewCreateSerializer(serializers.ModelSerializer):
     class Meta:
--- WEB(BE)/locations/views.py
+++ WEB(BE)/locations/views.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from accounts.models import Profile
 
 from locations.models import Location, Review
-from locations.serializers import LocationDetailSerializer, LocationListSerializer, ReviewSerializer, ReviewCreateSerializer
+from locations.serializers import LocationDetailSerializer, LocationListSerializer, ReviewListSerializer, ReviewDetailSerializer, ReviewCreateSerializer
 from locations.permissions import ReviewPermission
 
 
@@ -36,30 +36,7 @@ class LocationViewSet(viewsets.ReadOnlyModelViewSet):
         
         serializer = self.get_serializer(page, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-#class LocationUserStar(APIView)
-
-class ReviewViewSet(viewsets.ModelViewSet):
-    permission_classes = [ReviewPermission]
-    filter_backends = [DjangoFilterBackend]
-    pagination_class = None
     
-    def get_queryset(self):
-        query_set = Review.objects.filter(location=self.kwargs['location_id'])
-        return query_set.order_by('-created_at')
-
-    def get_serializer_class(self):
-        if self.action == ('list' or 'retrieve'):
-            return ReviewSerializer
-        return ReviewCreateSerializer
-    
-    def perform_create(self, serializer):
-        location = Location.objects.get(id=self.kwargs['location_id'])
-        profile = Profile.objects.get(user=self.request.user)
-        serializer.save(location=location, author=self.request.user, profile=profile)
-    
-
-        
 @api_view(['patch'])
 @permission_classes([IsAuthenticated])
 def like_location(request, location_id):
@@ -71,4 +48,50 @@ def like_location(request, location_id):
 
     serializer = LocationDetailSerializer(location)
     return Response({'total_likes': serializer.data['total_likes']}, status=status.HTTP_200_OK)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    permission_classes = [ReviewPermission]
+    filter_backends = [DjangoFilterBackend]
+    pagination_class = None
+    
+    def get_queryset(self):
+        query_set = Review.objects.filter(location=self.kwargs['location_id'])
+        return query_set.order_by('-created_at')
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ReviewListSerializer
+        if self.action == 'retrieve':
+            return ReviewDetailSerializer
+        return ReviewCreateSerializer
+    
+    def perform_create(self, serializer):
+        location = Location.objects.get(id=self.kwargs['location_id'])
+        profile = Profile.objects.get(user=self.request.user)
+        serializer.save(location=location, author=self.request.user, profile=profile)
+    
+@api_view(['patch'])
+@permission_classes([IsAuthenticated])
+def like_review(request, location_id, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if request.user in review.likes.all():
+        review.likes.remove(request.user)
+    else:
+        review.likes.add(request.user)
+
+    serializer = ReviewDetailSerializer(review)
+    return Response({'total_likes': serializer.data['total_likes']}, status=status.HTTP_200_OK)
+
+# @api_view(['patch'])
+# @permission_classes([IsAuthenticated])
+# def star_location(request, location_id):
+#     location = get_object_or_404(Location, id=location_id)
+#     if request.user in location.likes.all():
+#         location.likes.remove(request.user)
+#     else:
+#         location.likes.add(request.user)
+
+#     serializer = LocationDetailSerializer(location)
+#     return Response({'total_likes': serializer.data['total_likes']}, status=status.HTTP_200_OK)
         
