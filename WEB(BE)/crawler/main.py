b6cc875ea1e487fe5280173e542e3d560c3dc3de
froggy-hyun <111727754+froggy-hyun@users.noreply.github.com>
2022-10-23 14:40:35 +0000

    feat: 크롤러 mou 오류 해결, mou + 좋아요,별점 구현 1. 크롤러에서 mou를 intodb할 시 안되는 문제 해결 2. 크롤러 '8.MOU_crawled_amount()'항목 추가 3. 크롤러 try~except문 제거(오류 파악 쉽게) 4. 업소정보와 mou정보를 서로 다른 리스트에 저장하도록 수정

--- WEB(BE)/crawler/main.py
+++ WEB(BE)/crawler/main.py
@@ -26,121 +26,120 @@ processing = data_processing.processing()
 def result(crawled_data, temp):
 	return print("크롤링 완료(크롤링 한 데이터 %d개, 총 데이터 %d개)" %(len(temp), len(crawled_data)))
 
+def mou_result(mou_data, temp):
+    return print("mou크롤링 완료(크롤링 한 mou데이터 %d개, 총 데이터 %d개)" %(len(temp), len(mou_data)))
+
 while True:
     print('''0. ALL\n1. deogyanggu\n2. goseonggun\n3. hwacheongun\n4. yanggugun\n5. cheolwongun\n\
 6. uijeongbusi\n7. donghaesi\n8. sokchosi\n9. injegun\n10. pajusi\n11. hongcheongun\n12. pocheonsi\n\
 13. changwonsi\n14. nonsansi\n15. yeongcheonsi\n###MOU###\n99. MOUdata''')
     select = input("크롤링 할 웹페이지 선택(숫자), DB기능수행(db), 나가기(exit) : ")
     if select.isdigit():
-        try:
-            if select == "0":
-                temp = crawling.deogyanggu()
-                result(crawling.crawled_data, temp)
-                temp = crawling.goseonggun()
-                result(crawling.crawled_data, temp)
-                temp = crawling.hwacheongun()
-                result(crawling.crawled_data, temp)
-                temp = crawling.yanggugun()
-                temp = crawling.cheolwongun()
-                result(crawling.crawled_data, temp)
-                temp = crawling.uijeongbusi()
-                result(crawling.crawled_data, temp)
-                temp = crawling.donghaesi()
-                result(crawling.crawled_data, temp)
-                temp = crawling.sokchosi()
-                result(crawling.crawled_data, temp)
-                temp = crawling.injegun()
-                result(crawling.crawled_data, temp)
-                temp = crawling.pajusi()
-                result(crawling.crawled_data, temp)
-                temp = crawling.hongcheongun()
-                result(crawling.crawled_data, temp)
-                temp = crawling.pocheonsi()
-                result(crawling.crawled_data, temp)
-                temp = crawling.changwonsi()
-                result(crawling.crawled_data, temp)
-                temp = crawling.nonsansi()
-                result(crawling.crawled_data, temp)
-                temp = crawling.yeongcheonsi()
-                result(crawling.crawled_data, temp)
+        if select == "0":
+            temp = crawling.deogyanggu()
+            result(crawling.crawled_data, temp)
+            temp = crawling.goseonggun()
+            result(crawling.crawled_data, temp)
+            temp = crawling.hwacheongun()
+            result(crawling.crawled_data, temp)
+            temp = crawling.yanggugun()
+            temp = crawling.cheolwongun()
+            result(crawling.crawled_data, temp)
+            temp = crawling.uijeongbusi()
+            result(crawling.crawled_data, temp)
+            temp = crawling.donghaesi()
+            result(crawling.crawled_data, temp)
+            temp = crawling.sokchosi()
+            result(crawling.crawled_data, temp)
+            temp = crawling.injegun()
+            result(crawling.crawled_data, temp)
+            temp = crawling.pajusi()
+            result(crawling.crawled_data, temp)
+            temp = crawling.hongcheongun()
+            result(crawling.crawled_data, temp)
+            temp = crawling.pocheonsi()
+            result(crawling.crawled_data, temp)
+            temp = crawling.changwonsi()
+            result(crawling.crawled_data, temp)
+            temp = crawling.nonsansi()
+            result(crawling.crawled_data, temp)
+            temp = crawling.yeongcheonsi()
+            result(crawling.crawled_data, temp)
                 
-            elif select == "1":
-                temp = crawling.deogyanggu()
-                result(crawling.crawled_data, temp)
-            elif select == "2":
-                temp = crawling.goseonggun()
-                result(crawling.crawled_data, temp)
-            elif select == "3":
-                temp = crawling.hwacheongun()
-                result(crawling.crawled_data, temp)
-            elif select == "4":
-                temp = crawling.yanggugun()
-                result(crawling.crawled_data, temp)
-            elif select == "5":
-                temp = crawling.cheolwongun()
-                result(crawling.crawled_data, temp)
-            elif select == "6":
-                temp = crawling.uijeongbusi()
-                result(crawling.crawled_data, temp)
-            elif select == "7":
-                temp = crawling.donghaesi()
-                result(crawling.crawled_data, temp)
-            elif select == "8":
-                temp = crawling.sokchosi()
-                result(crawling.crawled_data, temp)
-            elif select == "9":
-                temp = crawling.injegun()
-                result(crawling.crawled_data, temp)
-            elif select == "10":
-                temp = crawling.pajusi()
-                result(crawling.crawled_data, temp)
-            elif select == "11":
-                temp = crawling.hongcheongun()
-                result(crawling.crawled_data, temp)
-            elif select == "12":
-                temp = crawling.pocheonsi()
-                result(crawling.crawled_data, temp)
-            elif select == "13":
-                temp = crawling.changwonsi()
-                result(crawling.crawled_data, temp)
-            elif select == "14":
-                temp = crawling.nonsansi()
-                result(crawling.crawled_data, temp)
-            elif select == "15":
-                temp = crawling.yeongcheonsi()
-                result(crawling.crawled_data, temp)
-            elif select == "99":
-                temp = crawling.MOUdata()
-                result(crawling.crawled_data, temp)
-            
-        except:
-            print('존재하지 않는 명령이거나 오류가 발생했습니다.')
+        elif select == "1":
+            temp = crawling.deogyanggu()
+            result(crawling.crawled_data, temp)
+        elif select == "2":
+            temp = crawling.goseonggun()
+            result(crawling.crawled_data, temp)
+        elif select == "3":
+            temp = crawling.hwacheongun()
+            result(crawling.crawled_data, temp)
+        elif select == "4":
+            temp = crawling.yanggugun()
+            result(crawling.crawled_data, temp)
+        elif select == "5":
+            temp = crawling.cheolwongun()
+            result(crawling.crawled_data, temp)
+        elif select == "6":
+            temp = crawling.uijeongbusi()
+            result(crawling.crawled_data, temp)
+        elif select == "7":
+            temp = crawling.donghaesi()
+            result(crawling.crawled_data, temp)
+        elif select == "8":
+            temp = crawling.sokchosi()
+            result(crawling.crawled_data, temp)
+        elif select == "9":
+            temp = crawling.injegun()
+            result(crawling.crawled_data, temp)
+        elif select == "10":
+            temp = crawling.pajusi()
+            result(crawling.crawled_data, temp)
+        elif select == "11":
+            temp = crawling.hongcheongun()
+            result(crawling.crawled_data, temp)
+        elif select == "12":
+            temp = crawling.pocheonsi()
+            result(crawling.crawled_data, temp)
+        elif select == "13":
+            temp = crawling.changwonsi()
+            result(crawling.crawled_data, temp)
+        elif select == "14":
+            temp = crawling.nonsansi()
+            result(crawling.crawled_data, temp)
+        elif select == "15":
+            temp = crawling.yeongcheonsi()
+            result(crawling.crawled_data, temp)
+        elif select == "99":
+            temp = crawling.MOUdata()
+            mou_result(crawling.crawled_mou_data, temp)
     if select == 'exit':
         break
         
     if select == 'db':
         while True:
-            print("1. db.creat_db()\n2. db.into_db()\n3. db.close_db()\n4. db.crawled_amount\n5. data_proccesing(카카오 REST API KEY 필수)\n###MOU\n6. MOU_create_db()\n7. MOU_into_db()")
+            print("1. db.creat_db()\n2. db.into_db()\n3. db.close_db()\n4. db.crawled_amount\n5. data_proccesing(카카오 REST API KEY 필수)\n###MOU\n6. MOU_create_db()\n7. MOU_into_db()\n8. MOU_crawled_amout()")
             select = input("기능수행(숫자), 뒤로가기(back) : ")
             if select.isdigit():
-                try:
-                    if select == "1":
-                        db.creat_db()
-                    elif select == "2":
-                        db.into_db(crawling.crawled_data)
-                    elif select == "3":
-                        db.close_db()
-                    elif select == "4":
-                        amount = db.crawled_amount()
-                        print('저장된 데이터는 %d개 입니다' %amount)
-                    elif select == "5":
-                        crawling.crawled_data = processing.return_address(crawling.crawled_data)
-                        crawling.crawled_data = processing.return_category(crawling.crawled_data)
-                    elif select == "6":
-                        db.MOU_create_db()
-                    elif select == "7":
-                        db.MOU_into_db(crawling.crawled_data)
-                except:
-                    print('존재하지 않는 명령이거나 오류가 발생했습니다.')
+                if select == "1":
+                    db.creat_db()
+                elif select == "2":
+                    db.into_db(crawling.crawled_data)
+                elif select == "3":
+                    db.close_db()
+                elif select == "4":
+                    amount = db.crawled_amount()
+                    print('저장된 데이터는 %d개 입니다' %amount)
+                elif select == "5":
+                    crawling.crawled_data = processing.return_address(crawling.crawled_data)
+                    crawling.crawled_data = processing.return_category(crawling.crawled_data)
+                elif select == "6":
+                    db.MOU_create_db()
+                elif select == "7":
+                    db.MOU_into_db(crawling.crawled_mou_data)
+                elif select == "8":
+                    amount = db.MOU_crawled_amout()
+                    print('저장된 데이터는 %d개 입니다' %amount)
             if select == 'back':
                 break
--- WEB(BE)/locations/urls.py
+++ WEB(BE)/locations/urls.py
@@ -3,14 +3,14 @@ from rest_framework import routers
 from locations import views
 
 router = routers.SimpleRouter()
-router.register('', views.LocationViewSet, basename='location') 
+router.register('location', views.LocationViewSet, basename='location') 
 router.register(r'(?P<location_id>[^/.]+)/reviews', views.ReviewViewSet, basename='review')
 router.register('mou', views.MouViewSet, basename='mou') 
 
 urlpatterns = [
-    path('<int:location_id>/like', views.like_location, name='location-like'),
-    path('<int:location_id>/star', views.star_location, name='location-star'),
-    path('<int:location_id>/reviews/<int:review_id>/like', views.like_review, name='review-like'),
+    path('location/<int:location_id>/like', views.like_location, name='location-like'),
+    path('location/<int:location_id>/star', views.star_location, name='location-star'),
+    path('location/<int:location_id>/reviews/<int:review_id>/like', views.like_review, name='review-like'),
 
     path('mou/<int:mou_id>/like', views.like_mou, name='mou-like'),
     path('mou/<int:mou_id>/star', views.star_mou, name='mou-star'),
