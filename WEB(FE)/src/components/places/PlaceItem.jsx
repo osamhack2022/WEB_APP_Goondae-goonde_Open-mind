4c10798e349fae23ae0d08f1e2c59aa370aafb7f
woong <inwoong100@gmail.com>
2022-10-24 00:29:56 +0900

    hotfix: 장소 이미지 다음 검색과 연동
    1. 장소 제목을 다음 이미지 검색 후 썸네일 URL 활용
    2. 세부 사항 확인 시 이미지 검색 내용 활용

--- WEB(FE)/src/components/places/PlaceItem.jsx
+++ WEB(FE)/src/components/places/PlaceItem.jsx
@@ -1,36 +1,41 @@
 import { Link } from 'react-router-dom';
+import LoadingPlaceItem from '../loading/LoadingPlaceItem';
 
-const PlaceItem = ({ location }) => {
-  return (
-    <Link to={`${location.id}`} className='group '>
-      <div className='aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8'>
-        <img
-          src={
-            location.imageSrc ||
-            'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg'
-          }
-          alt={location.imageAlt}
-          className='h-full w-full object-cover object-center group-hover:opacity-75'
-        />
-      </div>
-      <h3 className='mt-4 text-lg text-gray-900 group-hover:text-gray-400'>
-        {location.name}
-        <span className='ml-2 text-sm text-gray-600 group-hover:text-gray-300'>
-          {location.category}
-        </span>
-      </h3>
-      <p className='mt-1 text-sm font-normal text-gray-900  group-hover:text-gray-400'>
-        <span>{location.address}</span>
-      </p>
-      <p className='mt-1 text-sm font-normal text-gray-900  group-hover:text-gray-400'>
-        <span>{location.number}</span>
-      </p>
+const PlaceItem = ({ location, image }) => {
+  if (image) {
+    return (
+      <Link to={`${location.id}`} className='group '>
+        <div className='aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8'>
+          <img
+            src={
+              image[0].thumbnail_url ||
+              'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg'
+            }
+            alt={location.imageAlt}
+            className='h-full w-full object-cover object-center group-hover:opacity-75'
+          />
+        </div>
+        <h3 className='mt-4 text-lg text-gray-900 group-hover:text-gray-400'>
+          {location.name}
+          <span className='ml-2 text-sm text-gray-600 group-hover:text-gray-300'>
+            {location.category}
+          </span>
+        </h3>
+        <p className='mt-1 text-sm font-normal text-gray-900  group-hover:text-gray-400'>
+          <span>{location.address}</span>
+        </p>
+        <p className='mt-1 text-sm font-normal text-gray-900  group-hover:text-gray-400'>
+          <span>{location.number}</span>
+        </p>
 
-      <p className='mt-1 text-lg font-medium text-blue-400 group-hover:text-blue-200'>
-        {location.benefit}
-      </p>
-    </Link>
-  );
+        <p className='mt-1 text-lg font-medium text-blue-400 group-hover:text-blue-200'>
+          {location.benefit}
+        </p>
+      </Link>
+    );
+  } else {
+    return <LoadingPlaceItem />;
+  }
 };
 
 export default PlaceItem;
