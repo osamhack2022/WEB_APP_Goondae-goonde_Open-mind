b9feed4dda78ed4cf9e10023109a7942d7fc3def
woong <inwoong100@gmail.com>
2022-10-27 22:47:52 +0900

    hotfix: mou, tmo, 혜택 h2 추가

--- WEB(FE)/src/components/places/PlaceList.jsx
+++ WEB(FE)/src/components/places/PlaceList.jsx
@@ -1,11 +1,14 @@
 import PlaceItem from './PlaceItem';
-
+const h2 = {
+  location: '군 혜택',
+  mou: 'MOU 혜택',
+  tmo: 'TMO',
+};
 const PlaceList = ({ name, locations, images }) => {
   return (
     <div className='bg-white'>
       <div className='mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
-        <h2 className='sr-only'>{name}</h2>
-
+        <h2 className='mb-5 font-bold text-2xl'>{`⭐️ ${h2[name]}`}</h2>
         <div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 '>
           {locations.map((location, index) => (
             <PlaceItem
