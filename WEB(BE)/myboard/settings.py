be129a7b107e284a61e743121390ac5b6462841e
froggy-hyun <111727754+froggy-hyun@users.noreply.github.com>
2022-10-29 04:33:30 +0000

    hotfix: email, password, key 삭제 1.  email, password, key 삭제

--- WEB(BE)/myboard/settings.py
+++ WEB(BE)/myboard/settings.py
@@ -74,9 +74,9 @@ EMAIL_HOST = 'smtp.gmail.com' # 메일 호스트 서버
 
 EMAIL_PORT = '587' # gmail과 통신하는 포트
  
-EMAIL_HOST_USER = 'goondaegoonde@gmail.com' # 발신할 이메일
+EMAIL_HOST_USER = '*' # 발신할 이메일
 
-EMAIL_HOST_PASSWORD = 'dyfs cjcg pazq taby' # 발신할 메일의 비밀번호
+EMAIL_HOST_PASSWORD = '*' # 발신할 메일의 비밀번호
 
 EMAIL_USE_TLS = True # TLS 보안 방법
 
