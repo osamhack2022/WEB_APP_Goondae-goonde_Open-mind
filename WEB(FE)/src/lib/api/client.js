d17c285dfbf45442676279c65289a8d7f79ad90d
woong <inwoong100@gmail.com>
2022-10-29 17:48:58 +0900

    edit: readme introduce 이미지 추가

--- WEB(FE)/src/lib/api/client.js
+++ WEB(FE)/src/lib/api/client.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const client = axios.create();
 
-client.defaults.baseURL =
-  'http://ec2-52-79-185-182.ap-northeast-2.compute.amazonaws.com:8000';
+client.defaults.baseURL = 'http://127.0.0.1:8000';
 
 client.defaults.headers.common['Authorization'] = 'Bearer';
 
