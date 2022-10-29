f7926267aded87581447307dc8a9835264252350
woong <inwoong100@gmail.com>
2022-10-29 17:59:10 +0900

    hotfix: category breadcrumb 추가

--- WEB(FE)/src/components/places/PlaceList.jsx
+++ WEB(FE)/src/components/places/PlaceList.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import PlaceItem from './PlaceItem';
 
 const PlaceList = ({ name, locations, images }) => {
   const [h2, setH2] = useState({});
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get('category');
+  console.log(category);
   useEffect(() => {
     const userStorage = JSON.parse(localStorage.getItem('user'));
     setH2({
@@ -18,7 +22,9 @@ const PlaceList = ({ name, locations, images }) => {
   return (
     <div className='bg-white min-h-[64.5vh]'>
       <div className='mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
-        <h2 className='mb-5 font-bold text-2xl'>{`⭐️ ${h2[name]}`}</h2>
+        <h2 className='mb-5 font-bold text-2xl'>
+          {category ? `⭐️ ${h2[name]} / ${category}` : `⭐️ ${h2[name]}`}
+        </h2>
         <div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 '>
           {locations.map((location, index) => (
             <PlaceItem
