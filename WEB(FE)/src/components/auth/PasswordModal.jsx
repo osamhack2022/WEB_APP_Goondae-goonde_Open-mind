4fdb7d710f738c11973f46cf6782e31aa9691f4b
woong <inwoong100@gmail.com>
2022-10-30 10:27:03 +0900

    edit: readme 내 영상 링크 수정

--- WEB(FE)/src/components/auth/PasswordModal.jsx
+++ WEB(FE)/src/components/auth/PasswordModal.jsx
@@ -97,7 +97,7 @@ const PasswordModal = ({ visible, setVisible, handleClick, error }) => {
                           <button
                             type='submit'
                             onClick={onClick}
-                            className='mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                            className='mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-green-600 py-3 px-8 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2'
                           >
                             변경하기
                           </button>
